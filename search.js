e8f54802e7f081c397346b81f64a6802a9b221fa
dellsystem <ilostwaldo@gmail.com>
2011-05-16 10:47:49 -0400

    attempt to get many highlight boxes showing ... not working, temp commit

--- search.js
+++ search.js
@@ -1,25 +1,30 @@
 $('#highlight-button').click(function() {
     // Fire off an ajax request
     console.log("starting ajax request");
-    $.getJSON('search/random', function(data) {
+    $.getJSON('search/many', function(data) {
         
         // If the highlight div already exists, remove it
         var box = $('#highlight');
         if (box.length > 0) {
             box.remove();
         }
+        
+        var numBoxes = data.length;
+        var i, pageTopOffset, width, height, xStart, yStart, toAppend;
+        var stringBuilder = [];
+        for (var i = 0; i < numBoxes; i++) {
+            page = data[i].p;
+            console.log('page:' + page);
+            pageTopOffset = $('#page-' + page).position().top;
 
-        var x_start = data['x'];
-        var y_start = data['y'];
-        var width = data['w'];
-        var height = data['h'];
-
-        var to_append = '<div id="highlight" style="width: ' + width + '; height: ' + height + '; left: ' + x_start + '; top: ' + y_start + ';"></div>';
-        $('#image').append(to_append);
+            xStart = data[i].x;
+            yStart = data[i].y + pageTopOffset;
+            width = data[i].w;
+            height = data[i].h;
+            // Make the div that should be appended
+            toAppend = '<div id="highlight" style="width: ' + width + '; height: ' + height + '; left: ' + xStart + '; top: ' + yStart + ';"></div>';
+            stringBuilder.push(toAppend);
+        }
+        $('#documentpanel').append(stringBuilder.join(''));
     });
-});
-
-$(document).ready(function() {
-    //alert("WTF");
-    //console.log("lol");
 });
