45cc5556435bea2ba2463a7d5ee71af5c369c971
dellsystem <ilostwaldo@gmail.com>
2011-05-27 20:05:43 -0400

    divaserve.php rewrite in progress

--- php/divaserve.php
+++ php/divaserve.php
@@ -1,29 +1,118 @@
 <?php
 
+function get_max_zoom_level($img_wid, $img_hei, $t_wid, $t_hei) {
+    $largest_dim = ($img_wid > $img_hei) ? $img_wid : $img_hei;
+    $t_dim = ($img_wid > $img_hei) ? $t_wid : $t_hei;
+
+    $zoom_levels = ceil(log(($largest_dim + 1)/($t_dim + 1), 2));
+    return intval($zoom_levels);
+}
+
+function incorporate_zoom($img_dim, $zoom_difference) {
+    return $img_dim / pow(2, $zoom_difference);
+}
+
 // Image directory. Images in this directory will be served by folder name,
 // so if you have a folder named ...images/old_manuscript, a request for 
 // divaserve.php?d=old_manuscript will use the images in that directory.
-$IMAGE_DIR = "/Users/laurent/data/in_pyr";
+$IMAGE_DIR = "/mnt/images";
 $CACHE_DIR = "/tmp/diva.js";
 $IIP_SERVER = "http://localhost/cgi-bin/iipsrv.fcgi?";
 
 ### Nothing should change past here.
-$fid = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['d']);
-$zlv = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['z']);
-
-// where we will store the text files.
-$imgcache = $CACHE_DIR . "/" . $fid;
-$imgdir = $IMAGE_DIR . "/" . $fid;
-
-if(!file_exists($imgcache)){
-    mkdir($imgcache);
+if (!isset($_GET['d']) || !isset($_GET['z'])) {
+    die("Missing params");
 }
 
-$gfile = $imgcache . "/" . "docdata.txt";
+$dir = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['d']);
+$zoom = preg_replace('/[^0-9]/', '', $_GET['z']);
 
-if($handle = opendir($imgdir)) {
-    $imginfos = array();
-    $filenum = 0;
+$t_wid_get = (isset($_GET['t_w'])) ? $_GET['t_w'] : 0;
+$t_hei_get = (isset($_GET['t_h'])) ? $_GET['t_h'] : 0;
+$t_wid = (intval($t_wid_get) > 0) ? intval($t_wid_get) : 256;
+$t_hei = (intval($t_hei_get) > 0) ? intval($t_hei_get) : 256;
+
+// where we will store the text files.
+$img_cache = $CACHE_DIR . "/" . $dir;
+$img_dir = $IMAGE_DIR . "/" . $dir;
+
+$cache_file = $img_cache . "/" . "docdata.txt";
+$pgs = array();
+
+if (!file_exists($img_cache)) {
+    // Now go through the image directory and calculate stuff
+    mkdir($img_cache);
+    $images = array();
+    $lowest_max_zoom = 0;
+    foreach (glob($img_dir . '/*.tif') as $img_file) {
+        $img_size = getimagesize($img_file);
+        $img_wid = $img_size[0];
+        $img_hei = $img_size[1];
+        
+        $max_zoom = get_max_zoom_level($img_wid, $img_hei, $t_wid, $t_hei);
+        $lowest_max_zoom = ($lowest_max_zoom > $max_zoom || $lowest_max_zoom == 0) ? $max_zoom : $lowest_max_zoom;
+        
+        // Get the number from the filename (between the last _ and .)
+        $img_num = intval(substr($img_file, strrpos($img_file, '_') + 1, strrpos($img_file, '.') - strrpos($img_file, '_') - 1));
+        
+        // Figure out the image filename
+        $img_fn = substr($img_file, strrpos($img_file, '/') + 1);
+
+        $images[$img_num] = array(
+            'mx_h'      => $img_hei,
+            'mx_w'      => $img_wid,
+            'mx_z'      => $max_zoom,
+            'fn'        => $img_fn,
+        );
+    }
+    
+    // Now go through them again, store in $pgs
+    for ($i = 0; $i < count($images); $i++) {
+        if (array_key_exists($i, $images)) {
+            $h = incorporate_zoom($images[$i]['mx_h'], $lowest_max_zoom - $zoom);
+            $w = incorporate_zoom($images[$i]['mx_w'], $lowest_max_zoom - $zoom);
+            $c = ceil($w / $t_wid);
+            $r = ceil($h / $t_hei);
+            $m_z = $images[$i]['mx_z'];
+            $fn = $images[$i]['fn'];
+            $pgs[] = array(
+                'c'     => $c,
+                'r'     => $r,
+                'h'     => $h,
+                'w'     => $w,
+                'm_z'   => $m_z,
+                'fn'    => $fn, 
+            );
+        }
+    }
+} else {
+    // Assume that the cache directory already contains everything we need
+}
+/*
+// Calculate the dimensions
+$dims = array(
+    'a_wid'         => $a_wid,
+    'a_hei'         => $a_hei,
+    'mx_h'          => $mx_h,
+    'mx_w'          => $mx_w,
+    't_hei'         => $t_hei,
+    't_wid'         => $t_wid
+);*/
+
+// The full data to be returned
+$data = array(
+    'item_title'    => $dir,
+    'dims'          => '',//$dims,
+    'max_zoom'      => $lowest_max_zoom,
+    'pgs'           => $pgs
+);
+
+echo json_encode($data);
+
+/*
+if($handle = opendir($img_dir)) {
+    $img_info = array();
+    $file_num = 0;
     $max_res_number = 0;
     
     while (false !== ($file = readdir($handle))) {
@@ -196,5 +285,5 @@ $resp = array(
 );
 
 echo json_encode($resp);
-
+*/
 ?>
