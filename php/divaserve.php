8c412ee99ab9e781725aa74b8e99237351800519
dellsystem <ilostwaldo@gmail.com>
2011-06-03 18:35:43 -0400

    return to using floats for dimensions otherwise the total height is broken

--- php/divaserve.php
+++ php/divaserve.php
@@ -145,8 +145,8 @@ if (!file_exists($cache_file)) {
             $pgs[] = array(
                 'c'     => $c,
                 'r'     => $r,
-                'h'     => round($h),
-                'w'     => round($w),
+                'h'     => $h,
+                'w'     => $w,
                 'm_z'   => $m_z,
                 'fn'    => $fn, 
             );
