e866a5e68d5911385272850513ebf2893dc3247b
dellsystem <ilostwaldo@gmail.com>
2011-06-11 18:32:00 -0400

    add functioning tests for zoomIn and zoomOut functions

--- js/diva-tests.js
+++ js/diva-tests.js
@@ -8,6 +8,7 @@
         // First instantiate the element
         $('#diva-wrapper').diva({
             iipServerBaseUrl: "http://coltrane.music.mcgill.ca:9000/fcgi-bin/iipsrv.fcgi?FIF=/mnt/images/beromunster/",
+            zoomLevel: 2,
             backendServer: "http://petrucci.musiclibs.net:9002/divaserve.php?d=beromunster",
             onReady: function() {
                 // Only run tests after the document viewer has loaded
@@ -20,9 +21,6 @@
                     equals(dv.getItemTitle(), "Beromunster", "The title should be Beromunster");
                 });
 
-                test("getZoomLevel()", function() {
-                    equals(dv.getZoomLevel(), 2, "The initial zoom level should be 2");
-                });
 
                 test("getCurrentPage()", function() {
                     equals(dv.getCurrentPage(), 0, "The initial page index (not number) should be 0");
@@ -33,6 +31,43 @@
                     ok(!dv.gotoPage(0), "We shouldn't be able to go to page 0 (as 0 is not a valid page number)");
                     ok(dv.gotoPage(100), "We SHOULD be able to go to page 100");
                     equals(dv.getCurrentPage(), 99, "getCurrentPage() should now return 99");
+                    // Reset it to the initial page
+                    dv.gotoPage(0);
+                });
+
+                test("getZoomLevel()", function() {
+                    equals(dv.getZoomLevel(), 2, "The initial zoom level should be 2");
+                });
+
+                asyncTest("zoomIn(), callback and return value", function() {
+                    expect(2);
+                    var canZoomIn = dv.zoomIn(function(zoomLevel) {
+                        equals(zoomLevel, 3, "After zooming in, level should be 3");
+                        start();
+                    });
+
+                    ok(canZoomIn, "Should be able to zoom in");
+                });
+
+                // Unit tests shouldn't change state but it's hard to avoid in this case
+                asyncTest("zoomIn() again", function() {
+                    expect(2);
+                    var canZoomIn = dv.zoomIn(function(zoomLevel) {
+                        equals(zoomLevel, 4, "Should be 4 after zooming again");
+                        start();
+                    });
+
+                    ok(canZoomIn, "Should be able to zoom in");
+                });
+
+                asyncTest("zoomIn() when we can't zoom in", function() {
+                    expect(2);
+                    var canZoomIn = dv.zoomIn(function(zoomLevel) {
+                        equals(zoomLevel, 4, "Should still be 4");
+                        start();
+                    });
+
+                    ok(!canZoomIn, "Should not be able to zoom in");
                 });
             }
         });
