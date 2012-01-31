43e04988e0af5494250a01e89f6bc2979efc5752
dellsystem <ilostwaldo@gmail.com>
2012-01-31 12:07:39 -0500

    Line up comments in settings declarations

--- js/diva.js
+++ js/diva.js
@@ -56,8 +56,8 @@ THE SOFTWARE.
             onZoom: null,               // Callback function for zooming in general
             onZoomIn: null,             // Callback function for zooming in only
             onZoomOut: null,            // Callback function for zooming out only
-            pagesPerRow: 5,         // The default number of pages per row in grid view
-            tileFadeSpeed: 300,         // The tile fade-in speed in ms. Set to 0 to disable tile fading. May also be "fast" or "slow".
+            pagesPerRow: 5,             // The default number of pages per row in grid view
+            tileFadeSpeed: 300,         // The tile fade-in speed in ms (or "fast" or "slow"; 0 to disable)
             tileHeight: 256,            // The height of each tile, in pixels; usually 256
             tileWidth: 256,             // The width of each tile, in pixels; usually 256
             viewportMargin: 200,        // Pretend tiles +/- 200px away from viewport are in
