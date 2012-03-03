500b9483c1ea997847952bf8c2edb08d523606e9
dellsystem <ilostwaldo@gmail.com>
2012-03-02 22:21:18 -0500

    Increase default viewport margin from 200 to 400

--- js/diva.js
+++ js/diva.js
@@ -60,7 +60,7 @@ THE SOFTWARE.
             tileFadeSpeed: 300,         // The tile fade-in speed in ms (or "fast" or "slow"; 0 to disable)
             tileHeight: 256,            // The height of each tile, in pixels; usually 256
             tileWidth: 256,             // The width of each tile, in pixels; usually 256
-            viewportMargin: 200,        // Pretend tiles +/- 200px away from viewport are in
+            viewportMargin: 400,        // Pretend tiles +/- 400px away from viewport are in
             zoomLevel: 2                // The initial zoom level (used to store the current zoom level)
         };
         
