5f1a66b97b956f8b9ae82835b7879dbf6c2402b7
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-05-29 23:45:01 -0400

    Add anonymous function wrapper for plugins
    Close issue #102

--- source/js/plugins/download.js
+++ source/js/plugins/download.js
@@ -3,20 +3,22 @@ Download plugin for diva.js
 Allows you to download images served by IIPImage
 */
 
-window.divaPlugins.push((function() {
-    var settings = {};
-    return {
-        init: function(divaSettings) {
-            settings.iipServerURL = divaSettings.iipServerURL;
-        },
-        pluginName: 'download',
-        titleText: 'Download image at the given zoom level',
-        handleClick: function(event) {
-            var pageDiv = $(this).parent().parent();
-            var filename = $(pageDiv).attr('data-filename');
-            var width = $(pageDiv).width() - 1;
-            var image = settings.iipServerURL + filename + '&WID=' + width + '&CVT=JPG';
-            window.open(image);
-        }
-    }
-})());
+(function ($) {
+    window.divaPlugins.push((function() {
+        var settings = {};
+        return {
+            init: function(divaSettings) {
+                settings.iipServerURL = divaSettings.iipServerURL;
+            },
+            pluginName: 'download',
+            titleText: 'Download image at the given zoom level',
+            handleClick: function(event) {
+                var pageDiv = $(this).parent().parent();
+                var filename = $(pageDiv).attr('data-filename');
+                var width = $(pageDiv).width() - 1;
+                var image = settings.iipServerURL + filename + '&WID=' + width + '&CVT=JPG';
+                window.open(image);
+            }
+        };
+    })());
+})(jQuery);
