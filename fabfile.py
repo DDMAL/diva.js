3da7b6d310f5299095804dd71320d73f06a3f3a2
dellsystem <ilostwaldo@gmail.com>
2012-09-24 14:21:40 -0400

    Add basic release script

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,5 @@
+import os
+
 from fabric.api import *
 
 # Path to the Closure Compiler .jar file
@@ -31,3 +33,44 @@ def build():
 def test():
     with settings(warn_only=True):
         print local("phantomjs tests/run.js"),
+
+def release(version):
+    """
+    Creates a zip file containing just the files we need for the release.
+    Also adds the "latest version" text to diva.min.js.
+    """
+    files = {
+        'readme.md': 'readme.md',
+        'AUTHORS': 'AUTHORS',
+        'LICENSE': 'LICENSE',
+        'build/js/*': 'diva.js/js/',
+        'build/css/*': 'diva.js/css/',
+        'build/img/*': 'diva.js/img/',
+        'build/divaserve/*': 'dataservers/',
+        'source/processing/*': 'processing/',
+        'demo/*': 'examples/',
+    }
+
+    release_dir = "diva-%s" % version
+    try:
+        os.mkdir(release_dir)
+    except OSError:
+        # Empty the directory
+        try:
+            local("rm -r %s/*" % release_dir)
+        except OSError:
+            pass
+
+    # Copy all the files over
+    for source, dest in files.iteritems():
+        if dest.endswith('/'):
+            try:
+                os.makedirs(os.path.join(release_dir, dest))
+            except OSError:
+                # Already been created
+                pass
+
+        local("cp -R %s %s/%s" % (source, release_dir, dest))
+
+    local("tar czf %s.tar.gz %s" % (release_dir, release_dir))
+    local("zip %s.zip %s" % (release_dir, release_dir))
