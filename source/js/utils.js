3339ad2e38ab26e450f1dd0a2b1237188dd77885
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-07-19 21:40:24 -0500

    New: Add a publish/subscribe system for notifications

--- source/js/utils.js
+++ source/js/utils.js
@@ -698,3 +698,83 @@ $.fn.dragscrollable = function( options ){
 
 }(window.jQuery || window.Zepto));
 
+/**
+*      Events. Pub/Sub system for Loosely Coupled logic.
+*      Based on Peter Higgins' port from Dojo to jQuery
+*      https://github.com/phiggins42/bloody-jquery-plugins/blob/master/pubsub.js
+*
+*      Re-adapted to vanilla Javascript
+*
+*      @class Events
+*/
+var Events = (function (){
+    var cache = {},
+    /**
+     *      Events.publish
+     *      e.g.: Events.publish("/Article/added", [article], this);
+     *
+     *      @class Events
+     *      @method publish
+     *      @param topic {String}
+     *      @param args     {Array}
+     *      @param scope {Object} Optional
+     */
+    publish = function (topic, args, scope) {
+        if (cache[topic]) {
+            var thisTopic = cache[topic],
+                i = thisTopic.length - 1;
+
+            for (i; i >= 0; i -= 1) {
+                thisTopic[i].apply( scope || this, args || []);
+            }
+        }
+    },
+    /**
+     *      Events.subscribe
+     *      e.g.: Events.subscribe("/Article/added", Articles.validate)
+     *
+     *      @class Events
+     *      @method subscribe
+     *      @param topic {String}
+     *      @param callback {Function}
+     *      @return Event handler {Array}
+     */
+    subscribe = function (topic, callback) {
+        if (!cache[topic]) {
+            cache[topic] = [];
+        }
+        cache[topic].push(callback);
+        return [topic, callback];
+    },
+    /**
+     *      Events.unsubscribe
+     *      e.g.: var handle = Events.subscribe("/Article/added", Articles.validate);
+     *              Events.unsubscribe(handle);
+     *
+     *      @class Events
+     *      @method unsubscribe
+     *      @param handle {Array}
+     *      @param completely {Boolean}
+     *      @return {type description }
+     */
+    unsubscribe = function (handle, completely) {
+        var t = handle[0],
+            i = cache[t].length - 1;
+
+        if (cache[t]) {
+            for (i; i >= 0; i -= 1) {
+                if (cache[t][i] === handle[1]) {
+                    cache[t].splice(cache[t][i], 1);
+                    if(completely){ delete cache[t]; }
+                }
+            }
+        }
+    };
+
+    return {
+            publish: publish,
+            subscribe: subscribe,
+            unsubscribe: unsubscribe
+    };
+}());
+
