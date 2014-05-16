ada4ef32e2bc439e366922ed987fde26f2119263
Evan Magoni <evan.magoni@mail.mcgill.ca>
2014-05-16 15:18:11 -0400

    Fixed: Outdated parameters of diva test instance updated to reflect current settings

--- tests/utils.js
+++ tests/utils.js
@@ -5,10 +5,10 @@
     $.tempDiva = function (settings) {
         // If the divaserveURL, imageDir, iconPath and iipServerURL settings aren't defined, define them
         settings.divaserveURL = settings.divaserveURL || "http://petrucci.musiclibs.net:9002/demo/demo.php";
-        settings.imageDir = settings.imageDir || "beromunster";
-        settings.imageRoot = settings.imageRoot || "/mnt/images";
+        settings.imageDir = settings.imageDir || "/mnt/images/beromunster";
         settings.iipServerURL = settings.iipServerURL || "http://coltrane.music.mcgill.ca/fcgi-bin/iipsrv.fcgi";
         settings.iconPath = settings.iconPath || "../build/img/";
+        settings.objectData = settings.objectData || "/demo/beromunster.json";
 
         // First, empty it in case something else has been using it
         var dv = $('#diva-temp').data('diva');
