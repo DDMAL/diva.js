77970aeed792087877705892fcfb3d935cb13203
Andrew Horwitz <ahwitz@gmail.com>
2014-11-10 10:36:57 -0500

    New: automatically updates page indices. Supposedly.
    -Added more documentation
    
    -Added more functions

--- source/js/plugins/pagealias.js
+++ source/js/plugins/pagealias.js
@@ -17,7 +17,13 @@ This object does not need to have a complete list of possible page indices.
 
 2) a pageAliasFunction function that takes in a 0-indexed integer value (the 
 original page index) and returns either the aliased page index or the boolean
-value "false".
+value "false". For example, the default mapping from 0-index to 1-index may be 
+constructed as:
+
+    pageAliasFunction: function(originalPageIndex)
+    {
+        return originalPageIndex + 1;
+    }
 
 
 This plugin is solely for cosmetic purposes; it will replace the "Page _ of XX" 
@@ -28,6 +34,8 @@ attribute. If this fails, the plugin will then pass the original page index into
 the pageAliasFunction function. If this function returns a boolean "false" (NOT 
 a number 0 or an undefined), the plugin will revert to the default page index.
 
+The plugin may also be instantiated with the 'divaSettings.newTotalPages' 
+attribute, which will replace the "Page 1 of __" counter.
 */
 
 (function ($)
@@ -48,6 +56,40 @@ a number 0 or an undefined), the plugin will revert to the default page index.
                 {
                     divaSettings.pageAliasFunction = function(){return false;};
                 } 
+
+                if (divaSettings.newTotalPages === undefined)
+                {
+                    $(divaSettings.ID + 'num-pages').text(divaSettings.newTotalPages);
+                }
+                
+                /*
+                    Main function. Will return the first of these three that 
+                        resolves to boolean true:
+                    -Explicit alias as defined in pageAliases
+                    -Result of pageAliasFunction
+                    -originalPageIndex itself
+
+                    Else the function will return the result of isPageValid, 
+                        which is a safer way of returning false.
+                */
+                divaInstance.getAliasForPageIndex = function(originalPageIndex)
+                {
+                    return divaSettings.pageAliases[originalPageIndex] || divaSettings.pageAliasFunction(originalPageIndex) || originalPageIndex || divaInstance.isPageValid(originalPageIndex);
+                };
+
+                //Maps the current page index to getAliasForPageIndex
+                divaInstance.getCurrentAliasedPageIndex = function()
+                {
+                    return divaInstance.getAliasForPageIndex(divaSettings.currentPageIndex);
+                };
+
+                //unsubscribe the default currentPage updater
+                diva.Events.unsubscribe("VisiblePageDidChange", divaSettings.toolbar.updateCurrentPage);
+                        
+                updateCurrentAliasedPage = function ()
+                {
+                    document.getElementById(divaSettings.ID + 'current-page').textContent = divaInstance.getCurrentAliasedPageIndex();
+                };
             },
             pluginName: 'pagealias',
             titleText: 'Re-alias page indexes'
