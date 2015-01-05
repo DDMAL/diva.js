7eb7094a820f220481cfe0df746ed266cf2c0130
Andrew Horwitz <ahwitz@gmail.com>
2015-01-05 14:27:10 -0500

    New: manual scroll can be disabled while autoscroll is on
    -Converted scrollSpeed to pixels per second
    
    -Added support for disabling manual scroll while auto scroll is on
    
    -Added public setters for some variables, made these not functions anymore

--- source/js/plugins/autoscroll.js
+++ source/js/plugins/autoscroll.js
@@ -6,7 +6,7 @@ Lets Diva scroll in the primary direction (as determined by
 settings.horizontallyOriented) automatically at a given/changeable rate.
 
 Relevant settings:
-    -scrollSpeed: seconds to move 1000 pixels
+    -scrollSpeed: pixels per second (defaults to 10)
     -disableManualScroll: disables manual scroll while automatic scroll is on (defaults to false)
     -disableScrollEvents: automatic scroll will not trigger scrolling events (defaults to false)
     -currentlyAutoScrolling: whether or not autoscroll is currently on
@@ -28,6 +28,9 @@ Notes for Andrew:
             init: function(divaSettings, divaInstance)
             {
                 var pixelsPerScroll;
+                var disableManualScroll;
+                var autoScrollRefresh;
+                var scrollSpeed;
 
                 divaInstance.startScrolling = function()
                 {
@@ -37,18 +40,23 @@ Notes for Andrew:
                         return;
                     }
 
+                    if(disableManualScroll)
+                    {
+                        divaInstance.disableScrollable();
+                    }
+
                     divaSettings.currentlyAutoScrolling = true;
                     if(divaSettings.horizontallyOriented)
                     {
                         divaSettings.autoScrollInterval = setInterval(function(){
                             divaSettings.outerObject.scrollLeft(divaSettings.outerObject.scrollLeft() + pixelsPerScroll);
-                        }, divaSettings.autoScrollRefresh);
+                        }, autoScrollRefresh);
                     }
                     else
                     {
                         divaSettings.autoScrollInterval = setInterval(function(){
                             divaSettings.outerObject.scrollTop(divaSettings.outerObject.scrollTop() + pixelsPerScroll);
-                        }, divaSettings.autoScrollRefresh);
+                        }, autoScrollRefresh);
                     }
                 };
 
@@ -60,31 +68,75 @@ Notes for Andrew:
                         return;
                     }
 
+                    if(disableManualScroll)
+                    {
+                        divaInstance.enableScrollable();
+                    }
+
                     divaSettings.currentlyAutoScrolling = false;
                     clearInterval(divaSettings.autoScrollInterval);
                 };
 
+                divaInstance.changeRefresh = function(newRefresh)
+                {
+                    autoScrollRefresh = newRefresh;
+                    updatePixelsPerScroll();
+                };
+
                 divaInstance.changeScrollSpeed = function(newSpeed)
                 {
-                    divaSettings.scrollSpeed = newSpeed;
-                    pixelsPerScroll = 1000 / divaSettings.scrollSpeed / divaSettings.autoScrollRefresh;
-                            
+                    scrollSpeed = newSpeed;
+                    updatePixelsPerScroll();      
+                };
+
+                var updatePixelsPerScroll = function()
+                {
+                    pixelsPerScroll = scrollSpeed / (1000 / autoScrollRefresh);  
+                    
+                    //should be minimum of one otherwise it won't change the actual value
+                    if(pixelsPerScroll < 1)
+                    {
+                        autoScrollRefresh = autoScrollRefresh * (1 / pixelsPerScroll);
+                        pixelsPerScroll = scrollSpeed / (1000 / autoScrollRefresh); 
+                    }
+                };
+
+                divaInstance.disableManualScroll = function()
+                {
+                    disableManualScroll = true;
+                    if(divaSettings.currentlyAutoScrolling)
+                    {
+                        divaInstance.disableScrollable();
+                    }
+                };
+
+                divaInstance.enableManualScroll = function()
+                {
+                    disableManualScroll = false;
+                    if(divaSettings.currentlyAutoScrolling)
+                    {
+                        divaInstance.enableScrollable();
+                    }
+                };
+
+                divaInstance.disableScrollEvents = function()
+                {
+                    disableScrollEvents = true;
+                };
+
+                divaInstance.enableScrollEvents = function()
+                {
+                    disableScrollEvents = false;
                 };
 
                 divaSettings.currentlyAutoScrolling = false;
                 divaSettings.autoScrollInterval = "";
 
-                if(divaSettings.disableManualScroll === undefined)
-                    divaSettings.disableManualScroll = false;
-
-                if(divaSettings.disableScrollEvents === undefined)
-                    divaSettings.disableScrollEvents = false;
-
-                if(divaSettings.autoScrollRefresh === undefined)
-                    divaSettings.autoScrollRefresh = 10;
-
-                if(divaSettings.scrollSpeed === undefined)
-                    divaInstance.changeScrollSpeed(100);
+                disableManualScroll = divaSettings.disableManualScroll || false;
+                disableScrollEvents = divaSettings.disableScrollEvents || false;
+                autoScrollRefresh = divaSettings.autoScrollRefresh || 50;
+                
+                divaInstance.changeScrollSpeed((divaSettings.scrollSpeed || 10));
 
             },
             pluginName: 'autoscroll',
