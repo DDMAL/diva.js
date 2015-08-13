0138f15de0949d2ab1846ac9ab5d7240c2a10be3
Evan Magoni <esm367@gmail.com>
2015-08-13 16:07:56 -0400

    Refactoring: OR JSHint formatting suggestion

--- source/js/diva.js
+++ source/js/diva.js
@@ -2751,8 +2751,8 @@ window.divaPlugins = [];
             var data;
 
             // parse IIIF manifest if it is an IIIF manifest. TODO improve IIIF detection method
-            if (responseData.hasOwnProperty('@context') && (responseData['@context'].indexOf('iiif') !== -1
-                || responseData['@context'].indexOf('shared-canvas') !== -1))
+            if (responseData.hasOwnProperty('@context') && (responseData['@context'].indexOf('iiif') !== -1 ||
+                responseData['@context'].indexOf('shared-canvas') !== -1))
             {
                 settings.isIIIF = true;
                 data = parseManifest(responseData);
