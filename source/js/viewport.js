f7a6ad732adf44f6691756bfcbc0e23cd78b90b9
William Bain <bain.william.a@gmail.com>
2016-05-27 12:19:25 -0400

    Refactor: Drop dead code from viewport object
    When I wrote the viewport code I added a system where you could
    mark the viewport state as invalidated but defer the read of the
    updated values until a later point. I think I thought I needed
    that for zoom animations or something, but it really doesn't seem
    like the right approach.

--- source/js/viewport.js
+++ source/js/viewport.js
@@ -11,9 +11,6 @@ function Viewport(outer, options)
 
     this._top = this._left = this._width = this._height = null;
 
-    this._valid = false;
-    this._validationPending = false;
-
     this.invalidate();
 }
 
@@ -48,48 +45,11 @@ Viewport.prototype.hasHorizontalOverlap = function (region)
 
 Viewport.prototype.invalidate = function ()
 {
-    if (!this._setInvalid())
-        return;
-
-    this._revalidate();
-};
-
-// TODO(wabain): Get rid of this if it doesn't end up being used
-Viewport.prototype.invalidateAndDeferRevalidation = function ()
-{
-    if (!this._setInvalid())
-        throw new Error('cannot defer viewport revalidation: validation is already pending');
-
-    return this._revalidate.bind(this);
-};
-
-Viewport.prototype._setInvalid = function ()
-{
-    if (this._validationPending)
-        return false;
-
-    this._valid = false;
-    this._validationPending = true;
-
-    return true;
-};
-
-Viewport.prototype._revalidate = function ()
-{
     this._width = Math.min(this.outer.clientWidth, this.maxExtent);
     this._height = Math.min(this.outer.clientHeight, this.maxExtent);
 
     this._top = this.outer.scrollTop;
     this._left = this.outer.scrollLeft;
-
-    this._valid = true;
-    this._validationPending = false;
-};
-
-Viewport.prototype._assertValid = function (prop)
-{
-    if (!this._valid)
-        throw new TypeError('Access of viewport.' + prop + ' while it is invalidated');
 };
 
 ['top', 'left'].forEach(function (prop)
@@ -100,7 +60,6 @@ Viewport.prototype._assertValid = function (prop)
     Object.defineProperty(Viewport.prototype, prop, {
         get: function ()
         {
-            this._assertValid(prop);
             return this[privateProp];
         },
         set: function (newValue)
@@ -111,7 +70,6 @@ Viewport.prototype._assertValid = function (prop)
             // Viewport would need to know about the dimensions of the document,
             // and everything that uses these setters is working with knowledge of those
             // values anyway, so not much is gained by adding checks here.
-            this._assertValid(prop);
             this[privateProp] = this.outer[source] = newValue;
         }
     });
@@ -122,7 +80,6 @@ Viewport.prototype._assertValid = function (prop)
     Object.defineProperty(Viewport.prototype, prop, {
         get: function ()
         {
-            this._assertValid(prop);
             return this['_' + prop];
         }
     });
@@ -132,14 +89,12 @@ Object.defineProperties(Viewport.prototype, {
     bottom: {
         get: function ()
         {
-            this._assertValid('bottom');
             return this._top + this._height;
         }
     },
     right: {
         get: function ()
         {
-            this._assertValid('right');
             return this._left + this._width;
         }
     }
