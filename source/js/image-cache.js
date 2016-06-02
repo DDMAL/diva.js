ed398907f001544487059965094c8e810e4e0464
William Bain <bain.william.a@gmail.com>
2016-06-01 22:08:40 -0400

    New: Add ability to hold images in the cache
    Add the ability to hold cached images and ensure they are not
    removed, even if the cache overflows.

--- source/js/image-cache.js
+++ source/js/image-cache.js
@@ -14,6 +14,8 @@ function ImageCache(options)
 {
     options = options || { maxKeys: DEFAULT_MAX_KEYS };
     this.maxKeys = options.maxKeys || DEFAULT_MAX_KEYS;
+
+    this._held = {};
     this._urls = {};
     this._lru = [];
 }
@@ -36,10 +38,7 @@ ImageCache.prototype.put = function (url, img)
     {
         // FIXME: Does this make sense for this use case?
         record.img = img;
-
-        var index = this._lru.indexOf(record);
-        this._lru.splice(index, 1);
-        this._lru.unshift(record);
+        this._promote(record);
     }
     else
     {
@@ -49,14 +48,68 @@ ImageCache.prototype.put = function (url, img)
         };
 
         this._urls[url] = record;
-
-        if (this._lru.length === this.maxKeys)
-        {
-            var evicted = this._lru.pop();
-            delete this._urls[evicted.url];
-            debug('Evicting image %s', evicted.url);
-        }
-
+        this._tryEvict(1);
         this._lru.unshift(record);
     }
+};
+
+ImageCache.prototype._promote = function (record)
+{
+    var index = this._lru.indexOf(record);
+    this._lru.splice(index, 1);
+    this._lru.unshift(record);
+};
+
+ImageCache.prototype._tryEvict = function (extraCapacity)
+{
+    var allowedEntryCount = this.maxKeys - extraCapacity;
+
+    if (this._lru.length <= allowedEntryCount)
+        return;
+
+    var evictionIndex = this._lru.length - 1;
+
+    for (;;)
+    {
+        var target = this._lru[evictionIndex];
+
+        if (!this._held[target.url])
+        {
+            debug('Evicting image %s', target.url);
+            this._lru.splice(evictionIndex, 1);
+            delete this._urls[target.url];
+
+            if (this._lru.length <= allowedEntryCount)
+                break;
+        }
+
+        if (evictionIndex === 0)
+        {
+            /* istanbul ignore next */
+            debug.enabled && debug('Cache overfull by %s (all entries are being held)',
+                this._lru.length - allowedEntryCount);
+
+            break;
+        }
+
+        evictionIndex--;
+    }
+};
+
+ImageCache.prototype.acquire = function (url)
+{
+    this._held[url] = (this._held[url] || 0) + 1;
+    this._promote(this._urls[url]);
+};
+
+ImageCache.prototype.release = function (url)
+{
+    var count = this._held[url];
+
+    if (count > 1)
+        this._held[url]--;
+    else
+        delete this._held[url];
+
+    this._tryEvict(0);
 };
--- tests/unit/image-cache.js
+++ tests/unit/image-cache.js
@@ -54,3 +54,51 @@ QUnit.test('Put overwrites existing entries and bumps their LRU position', funct
     assert.strictEqual(cache.get('B'), null, 'B evicted');
     assert.strictEqual(cache.get('C'), imgC, 'C still in cache');
 });
+
+QUnit.test("Entries which have been acquire()'d are promoted", function (assert)
+{
+    var cache = new ImageCache({ maxKeys: 2 });
+
+    var imgA = new Image();
+    var imgB = new Image();
+    var imgC = new Image();
+
+    cache.put('A', imgA);
+    cache.put('B', imgB);
+
+    cache.acquire('A');
+    cache.release('A');
+
+    cache.put('C', imgC); // A would be evicted here if not promoted
+
+    assert.strictEqual(cache.has('A'), true, 'A remains');
+    assert.strictEqual(cache.has('B'), false, 'B evicted');
+    assert.strictEqual(cache.has('C'), true, 'C remains');
+});
+
+QUnit.test("Entries which have been acquire()'d are not evicted until released", function (assert)
+{
+    var cache = new ImageCache({ maxKeys: 2 });
+
+    var imgA = new Image();
+    var imgB = new Image();
+    var imgC = new Image();
+
+    cache.put('A', imgA);
+    cache.acquire('A');
+
+    cache.put('B', imgB);
+    cache.acquire('B');
+
+    cache.put('C', imgC); // A would be evicted here if not held
+
+    assert.strictEqual(cache.has('A'), true, 'A remains');
+    assert.strictEqual(cache.has('B'), true, 'B remains');
+    assert.strictEqual(cache.has('C'), true, 'C still entered');
+
+    cache.release('B'); // B will be evicted even though it still has priority over A
+
+    assert.strictEqual(cache.has('A'), true, 'A still remains (held)');
+    assert.strictEqual(cache.has('B'), false, 'B evicted');
+    assert.strictEqual(cache.has('C'), true, 'C still remains (by LRU policy)');
+});
