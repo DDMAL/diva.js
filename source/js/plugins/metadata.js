5c88ed74c6b0b0be82cf34bae4e93134af9bca0f
Eric Liu <ericliu404@gmail.com>
2018-08-13 16:48:35 -0400

    Fixed: Rename parser helper class

--- source/js/plugins/metadata.js
+++ source/js/plugins/metadata.js
@@ -20,7 +20,7 @@
  * Toolbar plugins must have a toolbarIcon and toolbarSide attribute, with toolbarSide being either 'left' or 'right'
  **/
 
-import parseLabelValue from '../utils/label-value-parser';
+import parseLabelValue from '../utils/parse-label-value';
 
 export default class MetadataPlugin
 {
