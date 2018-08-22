840927c183377d80d4f4a8f24fa7e2a2dd9686ba
Eric Liu <ericliu404@gmail.com>
2018-08-22 14:51:57 -0400

    Fix: Typo with array comparison

--- source/js/viewer-core.js
+++ source/js/viewer-core.js
@@ -1369,7 +1369,7 @@ export default class ViewerCore
             if (a.length !== b.length)
                 return false;
             
-            for (let i = 0, len = a.length; i < a; i++)
+            for (let i = 0, len = a.length; i < len; i++)
             {
                 if (a[i] !== b[i])
                     return false;
